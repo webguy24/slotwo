8fff560ca1b6db739a13431662cf68df08869264
webguy24 <65795729+webguy24@users.noreply.github.com>
2021-11-19 02:14:18 -0500

    Delete src directory

--- src/client/app/monk/monk.module.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    'use strict';
-
-    angular
-        .module('app.monk', [
-        	'app.progress',
-        	'app.message'
-        ]);
-})();
--- src/client/app/player/player.module.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    'use strict';
-
-    angular
-        .module('app.player', [
-        	'app.message',
-        	'app.progress'
-        ]);
-})();
